aa19012408d87dabe80be79dd10b3b2b2886b974
griger <gustavo.rg92@gmail.com>
2017-06-23 21:32:52 +0200

    Memoria finalizada.

--- graphs.R
+++ graphs.R
@@ -1,12 +1,28 @@
 library(ggplot2)
 
+# Representando gráficamente el desbalanceo entre clases
+
 iniciales = c(250,781,450)
 extras = c(1394, 3285, 2325)
 
-df <- data.frame(iniciales, extras, c('Tipo 1', 'Tipo 2', 'Tipo 3'))
-colnames(df) <- c('Iniciales', 'Extras', 'Tipo')
+total = c(extras, iniciales)
 
-imbalancedPlot <- ggplot(df, aes(Tipo)) + geom_bar(aes(fill = Iniciales + Extras)) + 
-  scale_x_continuous(name = "Tipo", breaks = c(0,1,2), labels = c("0" = "Tipo 1", "1" = "Tipo 2", "2" = "Tipo 3")) +
-  scale_fill_manual(name = "Conjunto", labels = c("Inicial", "Extra"), values =  c("khaki3", "rosybrown4"))                   + ylab("#pasajeros")
+df <- data.frame(total, rep(c('Tipo 1', 'Tipo 2', 'Tipo 3'), 2), c(rep("Extras", 3), rep("Base", 3)))
+colnames(df) <- c('Cantidad', 'Tipo', 'Extra')
+
+imbalancedPlot <- ggplot(df, aes(x = Tipo, y = Cantidad)) + geom_bar(stat="identity", aes(fill = factor(Extra))) + 
+  scale_x_discrete(name = "Tipo") +
+  scale_fill_manual(name = "Conjunto", labels = c("Inicial", "Extra"), values =  c("steelblue3", "steelblue4"))  + ylab("cantidad")
 imbalancedPlot
+
+# Representando la evolución del val_acc segun las epochs
+
+finetuning = c(0.56276, 0.56983, 0.57042, 0.57572, 0.58014, 0.58073, 0.58427, 0.58603, 0.59075, 0.59104, 0.59428, 0.59723, 0.59959)
+finetuningEpochs = c(0,1,6,9,18,25,29,33,36,42,43,48,49)
+
+df <- data.frame(finetuningEpochs, finetuning)
+colnames(df) <- c('Epoch', 'Val_acc')
+
+valAccPlot <- ggplot(df, aes(x = Epoch, y = Val_acc)) + geom_line(colour = "steelblue3", size = 1) 
+valAccPlot <- valAccPlot + geom_point(colour = "steelblue4", size = 3) +  ylim(0.4,0.8) + ylab("val_acc") + xlab("epoch")
+valAccPlot 
